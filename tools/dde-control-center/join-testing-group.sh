4eb3d0e7282f517e41a33a9b138172be2afe06bb
shenmo <jifengshenmo@outlook.com>
2024-09-25 18:15:21 +0800

    use garma

--- tools/dde-control-center/join-testing-group.sh
+++ tools/dde-control-center/join-testing-group.sh
@@ -7,13 +7,13 @@ load_transhell_debug
 HERE=$(dirname $(realpath $0))
 
 if check_is_installed gxde-testing-source ;then
-	if zenity --question --text="${TRANSHELL_CONTENT_QUIT_TEST_DIALOG}";then
+	if garma --question --text="${TRANSHELL_CONTENT_QUIT_TEST_DIALOG}";then
 		gxde-app-uninstaller package-name gxde-testing-source 
 	else
 		exit
 	fi
 else
-    zenity --warning --text="${TRANSHELL_CONTENT_JOIN_TEST_DIALOG}"
+    garma --warning --text="${TRANSHELL_CONTENT_JOIN_TEST_DIALOG}"
     if [[ "$?" = "0" ]];then
         gxde-app-installer gxde-testing-source 
         if ! check_is_installed gxde-testing-source ;then
